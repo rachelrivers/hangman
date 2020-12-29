4d93a037d21198751e5891083b10b23218a8201e
rachelrivers <rivers0723@gmail>
2020-12-28 22:31:07 -0500

    updates to guess mechanics..

--- index.js
+++ index.js
@@ -12,25 +12,69 @@ const wordBankTest = ["monkey", "cat"];
     const randomWord =  wordBankTest[Math.floor(Math.random() * wordBankTest.length)];
     const splitWord = randomWord.split(""); 
     const remainingLetters = randomWord.length;
-    
-    //get underscores
+
+  //hiding the word with underscores
     const hiddenAnswer = []; 
     for (let i = 0; i < splitWord.length; i++) {
      hiddenAnswer[i] = "_";
     }
 
-  //guessing mechanics
+console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
 
-  console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
+//GUESS SECTION, prompt and evaluate 
 
-  // while (remainingLetters > 0 && guessesLeft > 0) { 
-  //   console.log(hiddenAnswer.join(" "));
+while (remainingLetters > 0) {
+  console.log("Here is a word: " + hiddenAnswer);
+  let guess = prompt.question("Please guess a letter: ").toLowerCase();
+ 
+//making sure a guess is valid
 
-  //   let guess = prompt.question("Please guess a letter: ").toLowerCase(); 
-  // };
+if (guess === null) {
+  console.log("Please enter a letter")
+  } else { 
+  if (guess.length !== 1) {
+   console.log("Please enter a single letter.");
+  } else { 
+  if (correctLetters.includes(guess)) {
+  console.log("Letter has already been added, please guess a new letter.");
+  } 
 
-  // //evaluate guess
-  // if (guess.length !== 1) {
-  //   console.log("Please enter a single letter.");
-  // };
+// if (/[a-zA-Z]/.test(guess)) {
+// console.log("This is NOT a letter");
+//  }
 
+
+
+//a correct letter guess, push to the hidden array
+splitWord.forEach((letter, position) => {
+
+if (guess === letter) {
+  console.log("found", position); 
+  hiddenAnswer[position] = guess;
+} 
+console.log(hiddenAnswer)
+})
+
+
+//to add later, note* actual hangman graphic needs to be ABOVE the while loop
+
+// const hangGuy = () => {
+// if (guessesLeft == 6) {
+//   console.log("|"); 
+// }
+// if (guessesLeft == 5) {
+//   console.log("| /n 0");
+// }
+// if (guessesLeft == 4) {
+//   console.log("| /n0/n\|"); 
+// }
+// if (guessesLeft == 3) {
+//   console.log("| /n0/n\|/");
+// }
+// if (guessesLeft == 2) {
+//   console.log("| /n0/n \|/ /n /");
+// }
+// if (guessesLeft == 1) {
+//   console.log("| /n 0 /n \|/ /n / \");
+// }
+// }; 
