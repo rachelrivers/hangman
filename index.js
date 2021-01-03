9e4086841625d14f7852e8a81462055c08ed8046
rachelrivers <rivers0723@gmail>
2021-01-02 21:07:32 -0500

    rearrange order

--- index.js
+++ index.js
@@ -26,9 +26,18 @@ console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
 while (remainingLetters > 0) {
   console.log("Here is a word: " + hiddenAnswer);
   let guess = prompt.question("Please guess a letter: ").toLowerCase();
- 
-//making sure a guess is valid
+  evaluateGuess(guess);
+}
 
+////does the guess validation go in the while loop or as a separate function? 
+
+//function onlyLetters (input){ 
+// if (/[a-zA-Z]/.test(guess)) {
+// return false; 
+// console.log("This is NOT a letter")
+//  }
+
+const evaluateGuess = (guess) => { 
 if (guess === null) {
   console.log("Please enter a letter")
   } else { 
@@ -38,12 +47,9 @@ if (guess === null) {
   if (correctLetters.includes(guess)) {
   console.log("Letter has already been added, please guess a new letter.");
   } 
-
-// if (/[a-zA-Z]/.test(guess)) {
-// console.log("This is NOT a letter");
-//  }
-
-
+}
+  }
+}
 
 //a correct letter guess, push to the hidden array
 splitWord.forEach((letter, position) => {
@@ -55,26 +61,3 @@ if (guess === letter) {
 console.log(hiddenAnswer)
 })
 
-
-//to add later, note* actual hangman graphic needs to be ABOVE the while loop
-
-// const hangGuy = () => {
-// if (guessesLeft == 6) {
-//   console.log("|"); 
-// }
-// if (guessesLeft == 5) {
-//   console.log("| /n 0");
-// }
-// if (guessesLeft == 4) {
-//   console.log("| /n0/n\|"); 
-// }
-// if (guessesLeft == 3) {
-//   console.log("| /n0/n\|/");
-// }
-// if (guessesLeft == 2) {
-//   console.log("| /n0/n \|/ /n /");
-// }
-// if (guessesLeft == 1) {
-//   console.log("| /n 0 /n \|/ /n / \");
-// }
-// }; 
